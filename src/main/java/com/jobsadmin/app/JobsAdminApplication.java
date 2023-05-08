0044e2a70a4bbb74de6ad6e4799c4beb2ca13ea9
joseong-ug <hoho88@naver.com>
2023-05-09 06:50:36 +0900

    Start Project

---
+++ src/main/java/com/jobsadmin/app/JobsAdminApplication.java
@@ -0,0 +1,13 @@
+package com.jobsadmin.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JobsAdminApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JobsAdminApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jobsadmin/app/JobsAdminApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jobsadmin.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JobsAdminApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
